206085b51c80938d4efb65b44adaeae369b5034d
Chris Foral <chris.foral@gmail.com>
2016-05-09 14:49:17 -0500

    Brought methods over from the Sinatra app

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,57 @@
 class Movie < ActiveRecord::Base
+  
+  def movie_rank
+    x = {}
+    self.each do |m|
+      x[m.id] = m.movie_win_percent
+    end
+    movie_sorted_rank_arr = x.sort_by(&:last).reverse
+  end
+  
+  def total_rounds
+    (self.movie_upvotes) + (self.movie_downvotes)
+  end
+
+  def movie_upvotes
+    upvote = Vote.where({"upvote_id" => self.id})
+    upvote.count
+  end
+
+  def movie_downvotes
+    downvote = Vote.where({"downvote_id" => self.id})
+    downvote.count
+  end
+
+  def movie_win_percent
+    ((self.movie_upvotes.to_f / self.total_rounds.to_f) * 100).round(2)
+  end
+
+  def movie_loss_percent
+    ((self.movie_downvotes.to_f / self.total_rounds.to_f) * 100).round(2)
+  end
+
+  def movie_button_1_up
+    button_1_vote = Vote.where({"vote_button" => 1})
+    button_1_upvote = button_1_vote.where({"upvote_id" => self.id})
+    button_1_upvote.count
+  end
+
+  def movie_button_2_up
+    button_2_vote = Vote.where({"vote_button" => 2})
+    button_2_upvote = button_2_vote.where({"upvote_id" => self.id})
+    y.count
+  end
+
+  def movie_button_1_down
+    button_1_vote = Vote.where({"vote_button" => 1})
+    button_1_downvote = button_1_vote.where({"downvote_id" => self.id})
+    button_1_downvote.count
+  end
+
+  def movie_button_2_down
+    button_2_vote = Vote.where({"vote_button" => 2})
+    button_2_downvote = button_2_vote.where({"downvote_id" => self.id})
+    button_2_downvote.count
+  end
+
 end
