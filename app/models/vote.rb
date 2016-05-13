11c01975b1953844ed15440125981fc665a225a8
Chris Foral <chris.foral@gmail.com>
2016-05-12 23:15:47 -0500

    Fixed ranking algorithm to account not only for win percentage but number of voting rounds

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,7 +4,7 @@ class Vote < ActiveRecord::Base
   def self.movie_rank
     x = {}
     Movie.all.each do |movie|
-      x[movie.id] = movie.movie_win_percent
+      x[movie.id] = (movie.movie_win_percent * Vote.where("downvote_id" && "upvote_id" => movie.id).count)
     end
     movie_sorted_rank_arr = x.sort_by(&:last).reverse
   end
